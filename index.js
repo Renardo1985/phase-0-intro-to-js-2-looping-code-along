65ca24782d39f10ed9a6dee951fe83e7d6a4a175
Renardo Williams <rennwill@gmail.com>
2023-03-06 13:21:54 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+//const aTest = ["Charlie", "Samip", "Ali"];
+
+function writeCards(array,eventName)
+{    
+    const newarray = [];  
+    for (let i = 0; i < array.length; i++)
+    {    
+       newarray[i] = (`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newarray;
+}
+
+function countDown(num){
+    let count = num;
+    while (count >= 0){
+        console.log(count)
+        count--;
+    }
+}
